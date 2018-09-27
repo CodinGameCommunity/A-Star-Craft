eb3d6ef762425878f6c0945e1b48d8168e6d67e7
David Reignier <david.reignier@gmail.com>
2018-09-27 17:16:14 +0200

    Config TooltipModule correctly

--- src/main/resources/view/config.js
+++ src/main/resources/view/config.js
@@ -1,5 +1,5 @@
 import { GraphicEntityModule } from './entity-module/GraphicEntityModule.js';
-import { TooltipModule } from './modules/tooltip/TooltipModule.js';
+import { TooltipModule } from './TooltipModule.js';
 
 // List of viewer modules that you want to use in your game
 export const modules = [
