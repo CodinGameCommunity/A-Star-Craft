d79e90e76a9bddb926205b380f7fa790bd749016
David Reignier <david.reignier@gmail.com>
2018-09-27 17:41:22 +0200

    Remove TooltipModule because shit happens

--- src/main/resources/view/config.js
+++ src/main/resources/view/config.js
@@ -1,8 +1,6 @@
 import { GraphicEntityModule } from './entity-module/GraphicEntityModule.js';
-import { TooltipModule } from './TooltipModule.js';
 
 // List of viewer modules that you want to use in your game
 export const modules = [
-	GraphicEntityModule,
-	TooltipModule
+	GraphicEntityModule
 ];
