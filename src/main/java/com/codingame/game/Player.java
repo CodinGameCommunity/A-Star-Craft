5fb6ad166c367f1ca56e2126bd44b45bc2c0327f
David Reignier <david.reignier@gmail.com>
2018-09-30 14:08:59 +0200

    Oops, i wasn't supposed to commit that

--- src/main/java/com/codingame/game/Player.java
+++ src/main/java/com/codingame/game/Player.java
@@ -3,8 +3,11 @@ package com.codingame.game;
 import com.codingame.gameengine.core.AbstractSoloPlayer;
 
 public class Player extends AbstractSoloPlayer {
+
+    protected int expectedOutputLines = 1;
+
     public int getExpectedOutputLines() {
-        return 1;
+        return expectedOutputLines;
     }
 
     public void sendInputLine(Object line) {
